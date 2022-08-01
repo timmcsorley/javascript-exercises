4f6811081b27aa5af72a06a169372aff7dbf63f3
Tim <smajda85@gmail.com>
2022-08-01 19:59:41 -0400

    Complete first 3 JS testing exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let storedString = string;
+    let outputString = '';
+    if (num < 0){
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            outputString += storedString;
+        }
+        return outputString;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    inputString = inputString.toString();
+    //grab string length and store in a variable
+    let stringLength = inputString.length;
+    let reversedString = '';
+    //loop to add last character of string to new variable
+    for (i = 0; i <= stringLength; i++) {
+        reversedString += inputString.charAt(stringLength - i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
